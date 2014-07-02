7cbd3d47a74ff5488459223f855a297bb0c20235
Ado Matejov <a.matejov@centrum.sk>
2014-07-02 21:27:15 +0200

    initial

---
+++ Motor.cpp
@@ -0,0 +1,54 @@
+/*
+ * MotorV2.cpp
+ * This file is part of MotorV2
+ *
+ * Copyright (C) 2014, XLC Team
+ *
+ * Motor is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Motor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Motor; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+#include "MotorV2.h"
+
+MotorV2::MotorV2(int pin1, int pin2)
+{
+	pinMode(pin1,OUTPUT);
+	pinMode(pin2,OUTPUT);
+	_pin1 = pin1;
+	_pin2 = pin2;
+}
+
+void MotorV2::go(int speed)
+{
+	//use speed values only from range <0,255>
+	speed = abs(speed % 256);
+
+	//write direction
+    if (speed < 0){
+	    digitalWrite(_pin1, LOW);
+	    analogWrite(_pin2, speed);
+    } else {
+        analogWrite(_pin1, speed);
+        digitalWrite(_pin2, LOW);
+    }
+}
+
+void Motor::stop()
+{
+	digitalWrite(_pin1, LOW);
+    digitalWrite(_pin2, LOW);
+}
+
+
---
+++ Motor.h
@@ -0,0 +1,44 @@
+/*
+ * MotorV2.h - Library for controling DC motors connected to new drivers
+ * This file is part of Motor
+ *
+ * Copyright (C) 2014, XLC Team
+ *
+ * Motor is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Motor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Motor; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+
+#ifndef MotorV2_H
+#define MotorV2_H
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+class MotorV2
+{
+	public:
+		MotorV2(int pin1, int pin2);
+		void go(int speed);
+		void stop();
+	private:
+		int _pin1;
+		int _pin2;
+};
+
+#endif
